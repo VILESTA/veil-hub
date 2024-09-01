6d30b987431436542dca140b7dacea1dbc311e64
VILESTA <80594974+VILESTA@users.noreply.github.com>
2024-09-02 01:02:35 +0300

    upd

---
+++ lib/utils.ts
@@ -0,0 +1,7 @@
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+import { Active, DataRef, Over } from '@dnd-kit/core';
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
